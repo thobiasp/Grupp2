a26624b38d94431a1a42fe20b1e1e04c5785f4fb
Pontus.Ellboj <Pontus@172.20.200.187>
2015-10-27 11:42:52 +0100

    Changes in top of root

--- Hotel/src/main/RootClass.java
+++ Hotel/src/main/RootClass.java
@@ -1,8 +1,15 @@
 package main;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -16,6 +23,30 @@ public class RootClass extends Application {
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		BorderPane rootTop = new BorderPane();
+		
+		Image night = new Image("images/night.jpg");
+		ImageView nightView = new ImageView(night);
+		nightView.setFitHeight(150);
+		nightView.setFitWidth(800);
+		
+		HBox buttons = new HBox(10);
+		buttons.setAlignment(Pos.CENTER);
+		
+		Button food = new Button("FOOD");
+		Button spa = new Button("SPA");
+		Button transport = new Button("TRANSPORT");
+		Button houseKeeping = new Button("HOUSEKEEPING");
+		
+		VBox id = new VBox(10);
+		id.setAlignment(Pos.CENTER);
+		Label roomNumber = new Label();
+		
+		buttons.getChildren().addAll(food, spa, transport, houseKeeping);
+		rootTop.setCenter(nightView);
+		rootTop.setBottom(buttons);
+		root.setTop(rootTop);
 		
 	}
 
