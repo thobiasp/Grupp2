2024f6143bfb38a6fad2c28d05590cf4e112979a
thobiasp <thobias.pettersson@gmail.com>
2015-10-27 23:08:08 +0100

    added TreeMap, added 2 different date/time filters, added more function to 'Cancel'-button so it also removes the key in TreeMap

--- Hotel/src/main/RootClass.java
+++ Hotel/src/main/RootClass.java
@@ -1,8 +1,14 @@
 package main;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -32,16 +38,23 @@ import main.Booking.BType;
 
 public class RootClass extends Application {
 	
-	
+	String finalText;
 
 	@Override
 	public void start(Stage primaryStage) {
+		//format to show in listOfEvents
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM HH:mm");
+		//format to add to TreeMap so treemap can be sorted
+		DateFormat dateFormat2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
 		
 		BorderPane root = new BorderPane();
 		Scene scene = new Scene(root, 800, 600);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		TreeMap<String,String>mapForEvents = new TreeMap<>();
 		
 		//lists for 'upcomingEvents' center pane
 		ObservableList<HBox> obj=FXCollections.observableArrayList();
@@ -58,16 +71,16 @@ public class RootClass extends Application {
 		Label stack2 = new Label("Button 3");
 		Label stack3 = new Label("Button 4");
 		
-		
+
 		//Settings for 'upcoming events' BorderPane
 		BorderPane upcomingEvents = new BorderPane();			
 		VBox rightOfCenterPane = new VBox();
 		VBox leftOfCenterPane = new VBox();
-		rightOfCenterPane.setPrefWidth(300);
+		rightOfCenterPane.setPrefWidth(250);
 		leftOfCenterPane.setPrefWidth(300);
 		
 		//Test button for adding in 'upcoming events'
-		Button add = new Button("add obj");
+		Button add = new Button("test Book");
 		
 		//Add nodes to 'upcoming events' BorderPane
 		upcomingEvents.setRight(rightOfCenterPane);
@@ -81,24 +94,56 @@ public class RootClass extends Application {
 		//VBox for bottom pane in 'root' BorderPane
 		VBox bottomVBoxInRoot = new VBox();
 		Label head = new Label("Upcoming events");
+		HBox first = new HBox();
+		HBox second = new HBox();
+		HBox third = new HBox();
+		HBox forth = new HBox();
+		
 		head.setFont(Font.font(STYLESHEET_CASPIAN, 25));
-		bottomVBoxInRoot.getChildren().add(head);
+		bottomVBoxInRoot.getChildren().addAll(head,first,second,third,forth);
 	
 		
 		//event for button in 'upcomingEvents' bottom
 		add.setOnAction(event->{
-			LocalDateTime currenttime = LocalDateTime.now();
-			HBox hbox = new HBox(10);
-			Button x = new Button("X");
-			Label test1 = new Label(currenttime.getHour()+":"+currenttime.getSecond()+" fahf");
+			Date date = new Date();
 
-			hbox.getChildren().addAll(test1,x);
+			HBox hbox = new HBox(20);
+			
+			Button cancelButton = new Button("Cancel");
+			Label timeOfOrder= new Label(dateFormat.format(date));
+			Label test1 = new Label("Taxi");
+			Label price = new Label("150:-");
+			String dateToMap = dateFormat2.format(date);
+			String typeToMap=test1.getText();
+			
+			hbox.getChildren().addAll(timeOfOrder,test1,price,cancelButton);
 			obj.add(hbox);
 			
-			x.setOnAction(event2->{
+			//adding key:date and value:type  to TreeMap 'mapForEvents'
+			mapForEvents.put(dateToMap,typeToMap);
+			
+			
+			cancelButton.setOnAction(event2->{
 				obj.remove(hbox);
+				mapForEvents.remove(dateToMap);
 			});
+			
+			/*
+			Set<Entry<String,String>> set = mapForEvents.entrySet();
+			Iterator<Entry<String, String>> iterator = set.iterator();
+			finalText="";
+			while(iterator.hasNext()){
+				Entry<String, String> person = iterator.next();
+
+				finalText +=person.getKey()+" "+person.getValue()+"\n";
+			}
+
+			stack0.setText(finalText);*/
+					
+			
 		});
+		
+		
 		
 		
 		//creates mouse event for VBox in root
