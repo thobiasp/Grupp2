137f6012a8eaa98cc60cc79e941a58cb7a317d60
LisaWestberg <lisa.westberg@yh.nackademin.se>
2015-10-28 06:23:00 +0100

    Nya klasser: RoomService och MenuItem.

---
+++ Hotel/src/main/MenuItem.java
@@ -0,0 +1,50 @@
+package main;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class MenuItem {
+
+	enum Types {
+		FOOD, DRINK
+	}
+
+	private Types type;
+	private String name;
+	private String description;
+	private float price;
+	public MenuItem(Types type, String name, String description, float price) {
+		super();
+		this.type = type;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+	public Types getType() {
+		return type;
+	}
+	public void setType(Types type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	
+}
---
+++ Hotel/src/main/RoomService.java
@@ -0,0 +1,176 @@
+package main;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.TreeMap;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import main.MenuItem.Types;
+
+public class RoomService extends Application {
+
+	private ObservableList<MenuItem> menuData = null;
+	private ObservableList<MenuItem> pendingOrder = null;
+	Label totalLabel = new Label("Your total: ");
+	Float total = 0f;
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root);
+		HBox hbox1 = new HBox();
+		HBox hbox2 = new HBox();
+		HBox hbox3 = new HBox();
+		HBox hbox4 = new HBox();
+		HBox hbox5 = new HBox();
+		Label menuLabel = new Label("Room Service Menu");
+		TableView<MenuItem> menuView = new TableView<>();
+		TableColumn<MenuItem, String> itemNameCol = null;
+		TableColumn<MenuItem, String> itemDescriptionCol = null;
+		TableColumn<MenuItem, Float> itemPriceCol = null;
+		TableView<MenuItem> pendingView = new TableView<>();
+		TableColumn<MenuItem, String> itemNameCol2 = null;
+		TableColumn<MenuItem, Float> itemPriceCol2 = null;
+		
+		Button placeOrder = new Button("Place your order");
+		pendingOrder = FXCollections.observableArrayList();
+		menuData = FXCollections.observableArrayList();
+		TreeMap<Float,MenuItem> itemsOrdered = new TreeMap<>();
+
+		primaryStage.setTitle("Room Service");
+		scene.getStylesheets().addAll(this.getClass().getResource("roomService.css").toExternalForm());
+		root.setPrefSize(800, 300);
+		BorderPane.setMargin(hbox1, new Insets(5, 5, 5, 5));
+
+		hbox1.getChildren().add(menuLabel);
+		hbox1.setPrefSize(800, 30);
+		hbox1.setAlignment(Pos.CENTER_LEFT);
+		hbox1.setPadding(new Insets(5));
+
+		hbox2.setPrefSize(800, 220);
+		hbox2.setPadding(new Insets(5));
+		hbox2.getChildren().add(menuView);
+
+		hbox3.setPrefSize(600, 100);
+		hbox3.setPadding(new Insets(5));
+		hbox3.getChildren().add(pendingView); //List of orders to place goes here.
+
+		hbox4.setPrefSize(200, 100);
+		hbox4.setPadding(new Insets(10));
+		hbox4.getChildren().add(placeOrder);
+		this.populateMenu(10);
+
+		hbox5.setPrefSize(800, 100);
+		hbox5.setPadding(new Insets(5));
+		hbox5.getChildren().addAll(hbox3, hbox4);
+
+		itemNameCol = new TableColumn<>("");
+		itemNameCol.setResizable(false);
+		itemNameCol.setEditable(false);
+		itemNameCol.setPrefWidth(100);
+		itemNameCol.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
+
+		itemDescriptionCol = new TableColumn<>("");
+		itemDescriptionCol.setResizable(false);
+		itemDescriptionCol.setEditable(false);
+		itemDescriptionCol.setPrefWidth(625);
+		itemDescriptionCol.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("description"));
+
+		itemPriceCol = new TableColumn<>("Price");
+		itemPriceCol.setResizable(false);
+		itemPriceCol.setEditable(false);
+		itemPriceCol.setPrefWidth(75);
+		itemPriceCol.setCellValueFactory(new PropertyValueFactory<MenuItem, Float>("price"));
+
+		menuView.getColumns().add(itemNameCol);
+		menuView.getColumns().add(itemDescriptionCol);
+		menuView.getColumns().add(itemPriceCol);
+		menuView.getItems().addAll(menuData);
+		menuView.setItems(menuData);
+		
+		itemNameCol2 = new TableColumn<>("");
+		itemNameCol2.setResizable(false);
+		itemNameCol2.setEditable(false);
+		itemNameCol2.setPrefWidth(200);
+		itemNameCol2.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
+
+		itemPriceCol2 = new TableColumn<>("Price");
+		itemPriceCol2.setResizable(false);
+		itemPriceCol2.setEditable(false);
+		itemPriceCol2.setPrefWidth(75);
+		itemPriceCol2.setCellValueFactory(new PropertyValueFactory<MenuItem, Float>("price"));
+
+		pendingView.getColumns().add(itemNameCol2);
+		pendingView.getColumns().add(itemPriceCol2);
+		pendingView.getItems().addAll(pendingOrder);
+		pendingView.setItems(pendingOrder);
+
+		root.setTop(hbox1);
+		root.setCenter(hbox2);
+		root.setBottom(hbox5);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+        menuView.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+        	MenuItem item = menuView.getSelectionModel().getSelectedItem();
+            if (item != null) {
+                pendingOrder.add(item);
+            }
+        });
+        
+        pendingView.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+        	MenuItem item = menuView.getSelectionModel().getSelectedItem();
+            if (item != null) {
+                pendingOrder.remove(item);
+            }
+            	Platform.runLater(new Runnable() {
+        			@Override
+        			public void run() {
+        				ListIterator<MenuItem> it = pendingOrder.listIterator();
+        				float theTotal = 0f;
+        				while (it.hasNext()){
+        					MenuItem next = it.next();
+        					theTotal += next.getPrice();
+        				}
+        				totalLabel.setText("Your total :" + theTotal);	
+        			}
+        		});
+        });
+	}
+
+	private void populateMenu(int numItems) {
+		MenuItem food = null;
+		MenuItem drink = null;
+		for (int i = 1; i <= numItems; i++) {
+			food = new MenuItem(Types.FOOD, "Hamburger", "A big, juicy hamburger with lettuce and tomato.", 65f);
+			menuData.add(food);
+			i++;
+			drink = new MenuItem(Types.DRINK, "Corona", "Our best-selling lager!", 60f);
+			menuData.add(drink);
+		}
+		Comparator<MenuItem> byType = (MenuItem o1, MenuItem o2) -> (o1.getType()).compareTo(o2.getType());
+		Collections.sort(menuData, byType);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
